3725373af26fee5473e968a1275d397cf923eccb
dafoxia <reinhard.bramel@liwest.at>
2014-07-12 20:05:46 +0200

    make it more understandable

--- Interconnect.rb
+++ Interconnect.rb
@@ -27,14 +27,15 @@ require 'thread'
 require 'benchmark'
 
 class InterConnectBot
-	attr_reader :cli
-    def initialize botname, bitrate, host, port, tcpport
+	attr_reader :cli, :host, :port
+	attr_writer :awaytime, :disconnecttime
+    def initialize botname, bitrate, host, port
 		@host =	host
 		@port = port
-		
 		@myname = botname
-		@other = 0
-		# Runs on Mumble-Server and Server uses not standard port!
+		@awaytime = 60
+		@disconnecttime = 120
+		
 		@cli = Mumble::Client.new(host, port) do |conf|
         	conf.username = botname
             conf.password = ""
@@ -46,7 +47,6 @@ class InterConnectBot
 		@conn_and_join	= Queue.new
 		@create	= Queue.new
 		@mychilds = []
-		@tcpserver = TCPServer.new tcpport
 
     end
 
@@ -57,7 +57,8 @@ class InterConnectBot
 		@foreignport = foreignport
         @cli.connect
 		@away = away
-		sleep(1)
+		while !@cli.ready
+		end
         @cli.join_channel(channel)
 		@channelid = @cli.current_channel
 		@cli.on_text_message do |msg|
@@ -71,18 +72,6 @@ class InterConnectBot
 				end
 			end
 		end
-		
-		@cli.on_user_state do |state|
-
-		end
-	end
-	
-	def intercon_host
-		return @host
-	end
-	
-	def intercon_port
-		return @port
 	end
 	
     def get_ready 
@@ -99,8 +88,8 @@ class InterConnectBot
 	def termbots
 		@mychilds.each_with_index do |zeit, index|
 			if ( zeit != nil ) && @activebots[index].connected then
-				@activebots[index].join_channel(@away) if  ( ( Time.now - zeit ) >= 20 ) 								# go to away if 20 seconds no audio from user appeared
-				@activebots[index].disconnect if ( ( Time.now - zeit) >= 200 ) 											# disconnect bot if 200 seconds no audio from user appeared
+				@activebots[index].join_channel(@away) if  ( ( Time.now - zeit ) >= @awaytime )							# go to away if @awaytime seconds no audio from user appeared
+				@activebots[index].disconnect if ( ( Time.now - zeit) >= @disconnecttime ) 								# disconnect bot if @disconnecttime seconds no audio from user appeared
 			end
 		end
 		sleep 1
@@ -123,52 +112,52 @@ class InterConnectBot
 			index = @conn_and_join.pop																					# pop a user session number
 			if @activebots[index].connected == false then																# if bot not connected
 				@activebots[index].connect																				# connect it to server
-				sleep(1)																								# wait some time until we can join
+				while !@activebots[index].ready
+				end																										# wait until we can join
 				@activebots[index].mumble2mumble false																	# activate bot
 				msg = @activebots[index].get_imgmsg('./icons/m2muser.png')
 				@activebots[index].set_comment msg
 			end
 			while @activebots[index].current_channel == nil 
 				@activebots[index].join_channel(@homechannel)															# join channel
-				sleep 1
 			end
 		end
-		sleep 0.5
+		sleep 0.1
 	end
 	
 	def playit 
 		speakers = @cli.m2m_getspeakers
 		maxsize =0
 		x = Benchmark.measure { 
-		speakers.each do |sessionid|
-			if ( sessionid != nil ) then
-				if ( @cli.users.values_at(sessionid).[](0) != nil ) then
-					if ( @cli.users.values_at(sessionid).[](0).name[0..(@otherprefix.size - 1)] != @otherprefix ) then		# real user
-						if @activebots[sessionid] != nil then																# if bot exist
-							if ( @activebots[sessionid].connected ) then													# and connected
-								speakersize = @cli.m2m_getsize sessionid
-								maxsize =  speakersize if speakersize >= maxsize
-								if maxsize >= 2 then
-									@activebots[sessionid].join_channel(@homechannel) if @activebots[sessionid].current_channel != @homechannel 
-									frame1 = @cli.m2m_getframe sessionid											
-									frame2 = @cli.m2m_getframe sessionid	
-									if ( frame1.is_a? String ) && ( frame2.is_a? Integer ) then						
-										@activebots[sessionid].m2m_writeframe frame1 
-										@activebots[sessionid].m2m_writeframe frame2
-									else
-										puts "Packet LOST!"
+			speakers.each do |sessionid|
+				if ( sessionid != nil ) then
+					if ( @cli.users.values_at(sessionid).[](0) != nil ) then
+						if ( @cli.users.values_at(sessionid).[](0).name[0..(@otherprefix.size - 1)] != @otherprefix ) then		# real user
+							if @activebots[sessionid] != nil then																# if bot exist
+								if ( @activebots[sessionid].connected ) then													# and connected
+									speakersize = @cli.m2m_getsize sessionid
+									maxsize =  speakersize if speakersize >= maxsize
+									if maxsize >= 2 then
+										@activebots[sessionid].join_channel(@homechannel) if @activebots[sessionid].current_channel != @homechannel 
+										frame1 = @cli.m2m_getframe sessionid											
+										frame2 = @cli.m2m_getframe sessionid	
+										if ( frame1.is_a? String ) && ( frame2.is_a? Integer ) then						
+											@activebots[sessionid].m2m_writeframe frame1 
+											@activebots[sessionid].m2m_writeframe frame2
+										else
+											puts "Packet LOST!"
+										end
+										@mychilds[sessionid] = Time.now
 									end
-									@mychilds[sessionid] = Time.now
 								end
+								@conn_and_join << sessionid																		# of not connected - fill in in connect queue
+							else																								# if bot not exist
+								@create << sessionid																			# fill in create queue
 							end
-							@conn_and_join << sessionid																		# of not connected - fill in in connect queue
-						else																								# if bot not exist
-							@create << sessionid																			# fill in create queue
 						end
 					end
 				end
 			end
-		end
 		}
 		if ( 0.007 - x.real ) >= 0 then																						# full loop time should not exceed 0.01 s ( 10 ms)
 			sleep  ( 0.007 - x.real ) 																						# we'll keep it slightly shorter that we can hurry up if need
@@ -184,29 +173,42 @@ class InterConnectBot
 end
 
 
-@prefix0 = '_ICU_'
-@prefix1 = '_ICD_'
+@server1_name = "soa.chickenkiller.com"
+@server1_port = 64739
+@server1_bitrate = 0
+@server1_channel = "NatenomConnect"
+@server1_awaychan = "away"
+@server1_time2away = 60
+@server1_time2disconnect = 120
+@server1_BotName = '↯' +@server1_name + '↯'
 
-client0 = InterConnectBot.new @prefix0, 36000, "soa.chickenkiller.com", 64739, 6739					 	# Prefix is the Botname AND the prefix for each child! The number is the desired Bandwidth for this Bot for _UPLINK_!
-client1 = InterConnectBot.new @prefix1, 72000, "soa.chickenkiller.com", 64739, 6740						# Downlink Bandwidth we could not choose!
-sleep(1)
+@server2_name = "soa.chickenkiller.com"
+@server2_port = 64739
+@server2_bitrate = 0
+@server2_channel = "Interconnect"
+@server2_awaychan = "Interconnect"
+@server2_time2away = 60
+@server2_time2disconnect = 120
+@server2_BotName = 'ᛏ' +@server2_name + 'ᛏ'
 
-client0.connect 'uplink', 'downlink', 'away', client1.intercon_host, client1.intercon_port				# Bot connect from uplink to uplink foreign host -> Audio flows this way! Bot will send with 50kbps Opus-Audio to Client1 !!!
-client1.connect 'downlink', 'uplink', 'away', client0.intercon_host, client0.intercon_port				# Bot connect from uplink to uplink foreign host -> same flow direction as above. Sending with 72kbps Opus to Client0 !!!
-sleep (1)
+# BOT-NAME has to be different when spawn on one server!
 
-client0.get_ready 
-client1.get_ready
-sleep(1)
+client1 = InterConnectBot.new @server1_BotName, @server2_bitrate, @server1_name, @server1_port
+client2 = InterConnectBot.new @server2_BotName, @server1_bitrate, @server2_name, @server2_port
 
-client1.run @prefix0
-client0.run @prefix1
-sleep(1)
+client1.connect @server1_channel, @server2_channel, @server2_awaychan, client2.host, client2.port
+client2.connect @server2_channel, @server1_channel, @server1_awaychan, client1.host, client1.port
 
-msg0 = '<h1>Interconnect-Bot</h1><br />connected to ' + client1.intercon_host.to_s + ' Port: ' + client1.intercon_port.to_s + '<br />' + client0.cli.get_imgmsg('./icons/mumble2mumble256x256.png')
-msg1 = '<h1>Interconnect-Bot</h1><br />connected to ' + client0.intercon_host.to_s + ' Port: ' + client0.intercon_port.to_s + '<br />' + client0.cli.get_imgmsg('./icons/mumble2mumble256x256.png')
-client0.cli.set_comment msg0
+client1.get_ready 
+client2.get_ready
+
+client2.run @server1_BotName
+client1.run @server2_BotName
+
+msg1 = '<a href="mumble://' + client2.host.to_s + ':' + client2.port.to_s + '"><h1>Interconnect-Bot</h1></a>' + client2.cli.get_imgmsg('./icons/mumble2mumble256x256.png')
+msg2 = '<a href="mumble://' + client1.host.to_s + ':' + client1.port.to_s + '"><h1>Interconnect-Bot</h1></a>' + client1.cli.get_imgmsg('./icons/mumble2mumble256x256.png')
 client1.cli.set_comment msg1
+client2.cli.set_comment msg2
 puts "running...  ctrl-d to end!"
 
 begin
